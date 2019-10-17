4b94db2e60ff6b3405b0786978f5bad6b42fc73a
adehghan1376 <44525050+adehghan1376@users.noreply.github.com>
2019-10-17 20:41:16 +0330

    Hw1
    Successfully Done

---
+++ HW1 - Blinking led/program/core.c
@@ -0,0 +1,241 @@
+/*******************************************************
+This program was created by the
+CodeWizardAVR V3.12 Advanced
+Automatic Program Generator
+© Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Project : 
+Version : 
+Date    : 3/10/2019
+Author  : 
+Company : 
+Comments: 
+
+
+Chip type               : ATmega16
+Program type            : Application
+AVR Core Clock frequency: 1.000000 MHz
+Memory model            : Small
+External RAM size       : 0
+Data Stack size         : 256
+*******************************************************/
+
+#include <mega16.h>
+#include <delay.h>
+int step = 1;
+// Declare your global variables here
+//int x=0x00;
+//int level=1;
+//int delayTemp=0;
+// Declare your global variables here
+void wait(){
+      DDRA = 0x01;  //makes portC as output 
+      
+      while(1){
+        //x = ( x ? 0x00 : 0x01);
+        PORTA = PORTA ^ 0x01;
+        delay_ms(500);
+        
+      } 
+      
+}
+void blink(){
+      DDRA = 0x01;  //makes portC as output 
+      PORTA = PORTA ^ 0x01;
+      //delay_ms(500);
+}
+/*
+void goUp(){
+    DDRA = 0x02;
+    PORTA = 0x00;
+    delay_ms(3000);
+    DDRA = 0x00;
+    PORTA = 0x00;
+    level = level + 1;
+    PORTB=level;
+}
+void goDown(){
+    DDRA = 0x02;
+    PORTA = 0x02;
+    delay_ms(3000);
+    DDRA = 0x00;
+    PORTA = 0x00;
+    level = level - 1;
+    PORTB=level;
+}
+*/
+/*
+void go(int dir ){   //1 -> up , 0 -> down
+    PORTA = PORTA | 0x01;
+    DDRA = DDRA | 0x02;
+    (dir ? (PORTA = PORTA & 0xFD) : (PORTA = PORTA | 0x02));  
+    //                    1111 1101                 0000 0010
+    delay_ms(3000);
+    DDRA = DDRA & 0xfd ;
+    PORTA = 0x00;
+    level = level + (dir*2 -1);
+    PORTB=level;
+}
+*/
+// External Interrupt 0 service routine
+interrupt [EXT_INT0] void ext_int0_isr(void)
+{
+    step = step + 1;
+}
+interrupt [EXT_INT1] void ext_int1_isr(void)
+{
+    if(step > 1)
+        step = step - 1;
+}
+
+/*       OLD Int1
+interrupt [EXT_INT1] void ext_int1_isr(void)
+{
+    //go down interrupt
+// Place your code here
+    go(0);
+   // PORTC = PORTC ^ 0x02;
+}
+*/
+void main(void)
+{
+
+// Declare your local variables here
+
+// Input/Output Ports initialization
+// Port A initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRA=(0<<DDA7) | (0<<DDA6) | (0<<DDA5) | (0<<DDA4) | (0<<DDA3) | (0<<DDA2) | (0<<DDA1) | (0<<DDA0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTA=(0<<PORTA7) | (0<<PORTA6) | (0<<PORTA5) | (0<<PORTA4) | (0<<PORTA3) | (0<<PORTA2) | (0<<PORTA1) | (0<<PORTA0);
+
+// Port B initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=in Bit0=in 
+DDRB=(0<<DDB7) | (0<<DDB6) | (0<<DDB5) | (0<<DDB4) | (0<<DDB3) | (0<<DDB2) | (0<<DDB1) | (0<<DDB0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTB=(0<<PORTB7) | (0<<PORTB6) | (0<<PORTB5) | (0<<PORTB4) | (0<<PORTB3) | (0<<PORTB2) | (0<<PORTB1) | (0<<PORTB0);
+
+// Port C initialization
+// Function: Bit7=in Bit6=in Bit5=in Bit4=in Bit3=in Bit2=in Bit1=in Bit0=in 
+DDRC=(0<<DDC7) | (0<<DDC6) | (0<<DDC5) | (0<<DDC4) | (0<<DDC3) | (0<<DDC2) | (0<<DDC1) | (0<<DDC0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTC=(0<<PORTC7) | (0<<PORTC6) | (0<<PORTC5) | (0<<PORTC4) | (0<<PORTC3) | (0<<PORTC2) | (0<<PORTC1) | (0<<PORTC0);
+
+// Port D initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRD=(0<<DDD7) | (0<<DDD6) | (0<<DDD5) | (0<<DDD4) | (0<<DDD3) | (0<<DDD2) | (0<<DDD1) | (0<<DDD0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTD=(0<<PORTD7) | (0<<PORTD6) | (0<<PORTD5) | (0<<PORTD4) | (0<<PORTD3) | (0<<PORTD2) | (0<<PORTD1) | (0<<PORTD0);
+
+// Timer/Counter 0 initialization
+// Clock source: System Clock
+// Clock value: Timer 0 Stopped
+// Mode: Normal top=0xFF
+// OC0 output: Disconnected
+TCCR0=(0<<WGM00) | (0<<COM01) | (0<<COM00) | (0<<WGM01) | (0<<CS02) | (0<<CS01) | (0<<CS00);
+TCNT0=0x00;
+OCR0=0x00;
+
+// Timer/Counter 1 initialization
+// Clock source: System Clock
+// Clock value: Timer1 Stopped
+// Mode: Normal top=0xFFFF
+// OC1A output: Disconnected
+// OC1B output: Disconnected
+// Noise Canceler: Off
+// Input Capture on Falling Edge
+// Timer1 Overflow Interrupt: Off
+// Input Capture Interrupt: Off
+// Compare A Match Interrupt: Off
+// Compare B Match Interrupt: Off
+TCCR1A=(0<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (0<<WGM11) | (0<<WGM10);
+TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (0<<WGM12) | (0<<CS12) | (0<<CS11) | (0<<CS10);
+TCNT1H=0x00;
+TCNT1L=0x00;
+ICR1H=0x00;
+ICR1L=0x00;
+OCR1AH=0x00;
+OCR1AL=0x00;
+OCR1BH=0x00;
+OCR1BL=0x00;
+
+// Timer/Counter 2 initialization
+// Clock source: System Clock
+// Clock value: Timer2 Stopped
+// Mode: Normal top=0xFF
+// OC2 output: Disconnected
+ASSR=0<<AS2;
+TCCR2=(0<<PWM2) | (0<<COM21) | (0<<COM20) | (0<<CTC2) | (0<<CS22) | (0<<CS21) | (0<<CS20);
+TCNT2=0x00;
+OCR2=0x00;
+
+// Timer(s)/Counter(s) Interrupt(s) initialization
+TIMSK=(0<<OCIE2) | (0<<TOIE2) | (0<<TICIE1) | (0<<OCIE1A) | (0<<OCIE1B) | (0<<TOIE1) | (0<<OCIE0) | (0<<TOIE0);
+
+// External Interrupt(s) initialization
+// INT0: on
+// INT1: on
+// INT2: Off
+// INT0 Mode: Rising Edge
+MCUCR=(1<<ISC11) | (1<<ISC10) | (1<<ISC01) | (1<<ISC00);
+MCUCSR=(0<<ISC2);
+GICR=0xc0;  ////Enable External Interrupts INT0 and INT1
+//MCUCR = MCUCR | 0x0f;
+#asm("sei")
+
+// USART initialization
+// USART disabled
+UCSRB=(0<<RXCIE) | (0<<TXCIE) | (0<<UDRIE) | (0<<RXEN) | (0<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
+
+// Analog Comparator initialization
+// Analog Comparator: Off
+// The Analog Comparator's positive input is
+// connected to the AIN0 pin
+// The Analog Comparator's negative input is
+// connected to the AIN1 pin
+ACSR=(1<<ACD) | (0<<ACBG) | (0<<ACO) | (0<<ACI) | (0<<ACIE) | (0<<ACIC) | (0<<ACIS1) | (0<<ACIS0);
+SFIOR=(0<<ACME);
+
+// ADC initialization
+// ADC disabled
+ADCSRA=(0<<ADEN) | (0<<ADSC) | (0<<ADATE) | (0<<ADIF) | (0<<ADIE) | (0<<ADPS2) | (0<<ADPS1) | (0<<ADPS0);
+
+// SPI initialization
+// SPI disabled
+SPCR=(0<<SPIE) | (0<<SPE) | (0<<DORD) | (0<<MSTR) | (0<<CPOL) | (0<<CPHA) | (0<<SPR1) | (0<<SPR0);
+
+// TWI initialization
+// TWI disabled
+TWCR=(0<<TWEA) | (0<<TWSTA) | (0<<TWSTO) | (0<<TWEN) | (0<<TWIE);
+
+DDRA = 0x01;  //makes portC as output
+
+while (1)
+      {
+      
+      int mili = 0;  
+      int rate = 0;
+      // Place your code here
+      //if(PORTD & 0x80){
+      while (1){
+          int power = PIND;
+          power = power & 0x80;
+          if(power){ 
+            mili = mili + step;
+            rate = PIND;
+            rate = rate & 0x40;
+            if(mili>= (4000)){   
+                blink();
+                mili = 0;
+            }
+            delay_ms(1);        
+          }else{               
+            mili = 0;
+            PORTA = PORTA & 0xFE;
+          }
+          // Place your code here
+          }        
+      }
+}
+
